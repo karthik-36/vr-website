0e8522d8193292fd3966a66295389a4fd79e1f36
karthik singh <31167294+karthik-36@users.noreply.github.com>
2021-09-20 13:58:06 -0500

    documentation project 1

--- src/CustomTabs.js
+++ src/CustomTabs.js
@@ -94,46 +94,128 @@ export default function SimpleTabs() {
           </p>
           <br />
           <p>
-          <b>Venice:</b> The model's inspiration comes from a street in Venice with the iconic three-way lamps, a waterway with gondolas instead of roads with cars, and the St Mark's Campanile tower.
+            <b>Venice:</b> The model's inspiration comes from a street in Venice with the iconic three-way lamps, a waterway with gondolas instead of roads with cars, and the St Mark's Campanile tower.
           </p>
-          <br/>
+          <br />
           <p>
-          <b>Chernobyl:</b>  My original plan was to project a model of the abandoned Ferris wheel from Pripyat amusement park but not many people were aware of its existence and could not relate, so I went on and added a representation of the burning nuclear plant from "that incident".
+            <b>Chernobyl:</b>  My original plan was to project a model of the abandoned Ferris wheel from Pripyat amusement park but not many people were aware of its existence and could not relate, so I went on and added a representation of the burning nuclear plant from "that incident".
           </p>
-          <br/>
+          <br />
           <p>Along with the knick-knacks, The model for Chernobyl can also be projected in a ground plane after you click on the "switch ground plane" button. this will make a roughly 1-meter cube model appear in the ground which you could walk and navigate through using your phone.</p>
-          <br/>
+          <br />
           <h2 style={{ textAlign: "center", color: "blue" }}>Demo Video</h2>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <h2 style={{ textAlign: "center", color: "blue" }}>Source Code</h2>
-          <br/>
+          <br />
           <p><h3 style={{ color: "blue" }}> <a href="https://github.com/karthik-36/Knick-knack-AR">GitHub </a></h3></p>
- 
-          <br/>
+
+          <br />
           <h2 style={{ textAlign: "center", color: "blue" }}>Tools Used</h2>
-          <br/>
+          <br />
           <p><a href="https://unity.com/" target="_blank">Unity Game Engine (Version 2019.4.28f1)</a></p>
           <p><a href="https://developer.vuforia.com/downloads/sdk" target="_blank">Vuforia (Version 9.8)</a></p>
-          
-          <br/>
+
+          <br />
           <h2 style={{ textAlign: "center", color: "blue" }}>How to use</h2>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <h2 style={{ textAlign: "center", color: "blue" }}>Assets</h2>
-          <br/>
+          <br />
           <p>My own assets & list of all the assets that I <strike> stole  </strike>  borrowed from the internet.</p>
-          <br/>
-          <br/>
+          <br />
+          <ol>
+            <li><div>
+              <h3>Merge Cube - Venice</h3>
+              <ol>
+                <h4>Own models</h4>
+                <li>
+                Venice street lamps
+                </li>
+                <li>
+                St Mark's Campanile tower
+                </li>
+              </ol>
+
+              <br/>
+
+              <ol>
+                <h4>Internet models </h4>
+                <li>
+                <a href = "https://sketchfab.com/Hanesto">https://sketchfab.com/Hanesto</a>  : Multiple Venice buildings (homes)  by  Hanesto
+                </li>
+                <li>
+                <a href = " https://free3d.com/user/garleth93">https://free3d.com/user/garleth93</a>  : boat by garleth93
+                </li>
+                <li>
+                <a href = "https://free3d.com/user/andrijaalp">https://free3d.com/user/andrijaalp</a> : bridge by  andrijaalp
+                </li>
+                <li>
+                <a href = "mixamo.com">mixamo.com</a>  : couple & man on the boat
+                </li>
+              </ol>
+
+              <br/>
+
+              <ol>
+                <h4>Soundtrack </h4>
+                <li>
+                <a href = "https://www.youtube.com/watch?v=pOIBF-eIUEQ">https://www.youtube.com/watch?v=pOIBF-eIUEQ</a>  : Jesper Kyd - Back In Venice
+                </li>
+              </ol>
+              <br/>
+              <br/>
+            </div></li>
+
+            <li><div>
+              <h3>Class Cube - Chernobyl</h3>
+              <ol>
+                <h4>Own models</h4>
+                <li>
+                Ferris wheel
+                </li>
+                <li>
+                Burning nuclear reactor, Water hoses, Graphite on the ground
+                </li>
+              </ol>
+              <br/>
+              <ol>
+                <h4>Internet models</h4>
+                <li>
+
+                </li>
+                <li>
+
+                </li>
+                <li>
+
+                </li>
+                <li>
+
+                </li>
+              </ol>
+              <br/>
+              <ol>
+                <h4>Soundtrack</h4>
+                <li>
+                <a href = "https://www.youtube.com/watch?v=zxjjie1Ohio&ab_channel=JoJo%27sBizarreAdventures">https://www.youtube.com/watch?v=5AyhE26Hbls</a>  :  Stardust Crusaders OST - Omen
+               
+                </li>
+              </ol>
+            </div></li>
+          </ol>
+
+          <br />
+          <br />
           <h2 style={{ textAlign: "center", color: "blue" }}>Inspiration for the models</h2>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <h2 style={{ textAlign: "center", color: "blue" }}>Images/Phone Screenshots</h2>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <h2 style={{ textAlign: "center", color: "blue" }}>My thoughts on AR enhanced objects.</h2>
-          <br/>
-          <br/>
+          <br />
+          <br />
 
 
         </section>
