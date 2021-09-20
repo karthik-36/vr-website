5b0afee34a49df9afae21724622504f1ad4cf393
karthik singh <31167294+karthik-36@users.noreply.github.com>
2021-09-20 10:53:22 -0500

    project 1 structure

--- src/CustomTabs.js
+++ src/CustomTabs.js
@@ -7,7 +7,7 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { colors } from '@material-ui/core';
-import { orange } from '@material-ui/core/colors';
+import { blue, orange } from '@material-ui/core/colors';
 
 
 
@@ -55,6 +55,10 @@ const useStyles = makeStyles({
   tabs: {
     backgroundColor: orange
   },
+  section: {
+    marginLeft: "30%",
+    marginRight: "30%",
+  },
   label: {
   },
 });
@@ -71,7 +75,7 @@ export default function SimpleTabs() {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Tabs className={classes.root} style = {{backgroundColor : "#f58e00"}}  value={value} onChange={handleChange} aria-label="simple tabs example">
+        <Tabs className={classes.root} style={{ backgroundColor: "#f58e00" }} value={value} onChange={handleChange} aria-label="simple tabs example">
           <Tab label="Project 1" {...a11yProps(0)} />
           <Tab label="Project 2" {...a11yProps(1)} />
           <Tab label="Project 3" {...a11yProps(2)} />
@@ -79,17 +83,76 @@ export default function SimpleTabs() {
           <Tab label="Homework" {...a11yProps(4)} />
         </Tabs>
       </AppBar>
-      <TabPanel  value={value} index={0}>
-        Project 1:
+      <TabPanel value={value} index={0}>
+        <section >
+          <h1 style={{ textAlign: "center" }}>Project 1: Road to Nowhere</h1>
+          <br />
+          <br />
+          <h2 style={{ textAlign: "center", color: "blue" }}>Introduction</h2>
+          <br />
+          <p> We basically have 2 cubes in real life called the merge cube (figure 1) and class cube (figure 2). Using the patterns present on each side of the cube we can project a 3d model on top which in our case has to represent a place/city on earth. So, I am going to represent Venice and Chernobyl for merge cube and class cube respectively.
+          </p>
+          <br />
+          <p>
+          <b>Venice:</b> The model's inspiration comes from a street in Venice with the iconic three-way lamps, a waterway with gondolas instead of roads with cars, and the St Mark's Campanile tower.
+          </p>
+          <br/>
+          <p>
+          <b>Chernobyl:</b>  My original plan was to project a model of the abandoned Ferris wheel from Pripyat amusement park but not many people were aware of its existence and could not relate, so I went on and added a representation of the burning nuclear plant from "that incident".
+          </p>
+          <br/>
+          <p>Along with the knick-knacks, The model for Chernobyl can also be projected in a ground plane after you click on the "switch ground plane" button. this will make a roughly 1-meter cube model appear in the ground which you could walk and navigate through using your phone.</p>
+          <br/>
+          <h2 style={{ textAlign: "center", color: "blue" }}>Demo Video</h2>
+          <br/>
+          <br/>
+          <h2 style={{ textAlign: "center", color: "blue" }}>Source Code</h2>
+          <br/>
+          <p><h3 style={{ color: "blue" }}> <a href="https://github.com/karthik-36/Knick-knack-AR">GitHub </a></h3></p>
+ 
+          <br/>
+          <h2 style={{ textAlign: "center", color: "blue" }}>Tools Used</h2>
+          <br/>
+          <p><a href="https://unity.com/" target="_blank">Unity Game Engine (Version 2019.4.28f1)</a></p>
+          <p><a href="https://developer.vuforia.com/downloads/sdk" target="_blank">Vuforia (Version 9.8)</a></p>
+          
+          <br/>
+          <h2 style={{ textAlign: "center", color: "blue" }}>How to use</h2>
+          <br/>
+          <br/>
+          <h2 style={{ textAlign: "center", color: "blue" }}>Assets</h2>
+          <br/>
+          <p>My own assets & list of all the assets that I <strike> stole  </strike>  borrowed from the internet.</p>
+          <br/>
+          <br/>
+          <h2 style={{ textAlign: "center", color: "blue" }}>Inspiration for the models</h2>
+          <br/>
+          <br/>
+          <h2 style={{ textAlign: "center", color: "blue" }}>Images/Phone Screenshots</h2>
+          <br/>
+          <br/>
+          <h2 style={{ textAlign: "center", color: "blue" }}>My thoughts on AR enhanced objects.</h2>
+          <br/>
+          <br/>
+
+
+        </section>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Project 2:
+        <section >
+          <h1 style={{ textAlign: "center" }}>Project 2: </h1>
+        </section>
       </TabPanel>
       <TabPanel value={value} index={2}>
-        Project 3:
+        <section >
+          <h1 style={{ textAlign: "center" }}>Project 3: </h1>
+        </section>
       </TabPanel>
       <TabPanel value={value} index={3}>
-        Student's choice:
+
+        <section >
+          <h1 style={{ textAlign: "center" }}>Student's choice: </h1>
+        </section>
       </TabPanel>
       <TabPanel value={value} index={4}>
         Homework:
